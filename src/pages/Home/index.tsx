ba13dd6e7feda656fd811df9436ce1795be0208c
Peter Ricardo <peter.ricardo.pfz@gmail.com>
2022-04-08 08:18:42 -0300

    Initial project structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home(): JSX.Element {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="text-2xl font-bold underline">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <div>React E-commerce</div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
